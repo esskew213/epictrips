4cb15212bbc5e3585764d4c955e7520a3fbe765c
Sarahqtw <Sarahqtw@hotmail.com>
2022-04-22 17:06:20 +0800

    added getServerSideProps to pull trips from db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
 import Head from 'next/head';
 import React from 'react';
 import Image from 'next/image';
-const Home: React.FC = () => {
+import { GetServerSideProps } from 'next';
+import prisma from '../lib/prisma';
+import { TripCardProps } from '../components/TripCard';
+export const getServerSideProps: GetServerSideProps = async () => {
+  const trips = await prisma.trip.findMany({
+    where: { public: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  // need to do JSON parse / stringify as next cannot serialize datetime objects
+  return { props: { trips: JSON.parse(JSON.stringify(trips)) } };
+};
+
+type Props = {
+  trips: TripCardProps[];
+};
+
+const Home: React.FC<Props> = (props) => {
   return (
     <div className=''>
       <Head>
@@ -12,6 +32,11 @@ const Home: React.FC = () => {
 
       <main>
         <h1 className='text-3xl px-2 py-4 bg-slate-400'>Epic Trips</h1>
+        {props.trips.map((trip) => (
+          <div key={trip.id}>
+            {trip.title} in {trip.destination} by {trip.author.name}
+          </div>
+        ))}
       </main>
     </div>
   );
